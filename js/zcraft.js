5915c97f06da39fac63f5b73d4ce475cb66cf7f0
Amaury Carrade <amaury@carrade.eu>
2016-06-08 21:58:49 +0200

    Retrait d'une fonction JS inutilisée

--- js/zcraft.js
+++ js/zcraft.js
@@ -4,20 +4,6 @@ $(function()
 
     var server_ip = 'zcraft.fr';
 
-    var handle_ping = function(ping)
-    {
-        if (ping.status != "ok")
-        {
-            $('#zcraft-online-offline').show();
-            $('#zcraft-online-status').removeClass('unknown').addClass('offline');
-        }
-        else
-        {
-            console.log(ping);
-            $('#zcraft-online-count').text(ping.data.online_players + ' / ' + ping.data.max_players).show();
-            $('#zcraft-online-status').removeClass('unknown').addClass('online');
-        }
-    };
 
     // TODO si mise en prod, utiliser un service sur zcraft directement
     $.getJSON('https://amaury.carrade.eu/tools/minecraft/ping/json?ip=' + server_ip)
@@ -42,16 +28,16 @@ $(function()
                 $('#zcraft-online-count').text(ping.data.online_players + ' / ' + ping.data.max_players).show();
                 $('#zcraft-online-status').removeClass('unknown').addClass('online');
 
-                var players_list = '';
+                // Players sorted by lower-cased name
+                ping.data.players.sort(function (a, b) {
+                    return a.toLowerCase().localeCompare(b.toLowerCase());
+                });
 
-                for (var i in ping.data.players)
+                var players_list = '';
+                ping.data.players.forEach(function(player_name)
                 {
-                    if (ping.data.players.hasOwnProperty(i))
-                    {
-                        var player_name = ping.data.players[i];
-                        players_list += '<li title="' + player_name + '" ' + tooltip_attributes + '><img src="https://minotar.net/helm/' + player_name + '/28" alt="' + player_name + '" /></li>';
-                    }
-                }
+                    players_list += '<li title="' + player_name + '" ' + tooltip_attributes + '><img src="https://minotar.net/helm/' + player_name + '/28" alt="' + player_name + '" /></li>';
+                });
 
                 $online_list.append(players_list).show();
             }
