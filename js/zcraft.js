c02647f57fdb7047c1ce492b8b2fab7227b252b6
Amaury Carrade <amaury@carrade.eu>
2016-06-09 16:36:05 +0200

    Amélioration notable du menu mobile

--- js/zcraft.js
+++ js/zcraft.js
@@ -2,6 +2,22 @@ $(function()
 {
     'use strict';
 
+
+    //$.support.transition = false;
+
+    var $main_nav = $('#main-nav');
+
+    $(':not(#main-nav, .navbar-toggle)').click(function ()
+    {
+        setTimeout(function () {
+            if ($main_nav.hasClass('in') && !$main_nav.hasClass('collapsing'))
+            {
+                $main_nav.collapse('hide');
+            }
+        }, 20);
+    });
+
+
     var server_ip = 'zcraft.fr';
 
 
